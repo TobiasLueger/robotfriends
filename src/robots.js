10bf407a950251eb10b9bd02a054819691d3edd1
Tobias Lüger <44581179+TobiasLueger@users.noreply.github.com>
2018-11-15 11:49:37 +0100

    Uploaded project

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Lea Pete',
+		username: 'LeLe',
+		email: 'lea@test.com'
+	},
+	{
+		id: 2,
+		name: 'Toni Sep',
+		username: 'Toto',
+		email: 'toni@test.com'
+	},
+	{
+		id: 3,
+		name: 'Octavio Zeolla',
+		username: 'Ococ',
+		email: 'octavio@test.com'
+	},
+	{
+		id: 4,
+		name: 'Rafaela Bagnall',
+		username: 'Rafa',
+		email: 'Rafaela@test.com'
+	},
+	{
+		id: 5,
+		name: 'Salley Dong',
+		username: 'Salley',
+		email: 'salley@test.com'
+	},
+	{
+		id: 6,
+		name: 'Hedwig Stimson',
+		username: 'Hedwig',
+		email: 'hedwig@test.com'
+	},
+	{
+		id: 7,
+		name: 'Rogelio Febres',
+		username: 'Rogelio',
+		email: 'rogelio@test.com'
+	},
+	{
+		id: 8,
+		name: 'Virgilio Tedeschi',
+		username: 'Virgilio',
+		email: 'virgilio@test.com'
+	}
+];
