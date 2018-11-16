aa11c4bb5acc97e92465ecb618aa507e76146e69
Tobias Lüger <44581179+TobiasLueger@users.noreply.github.com>
2018-11-16 09:57:08 +0100

    added build files to display Webside on link

---
+++ precache-manifest.bdd3b11e5d7d2f4cb31fa5b0874f068d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b262aa26ffb039e61a3",
+    "url": "/static/js/main.6b262aa2.chunk.js"
+  },
+  {
+    "revision": "f0ebd11ae2b6c8b6a955",
+    "url": "/static/js/1.f0ebd11a.chunk.js"
+  },
+  {
+    "revision": "6b262aa26ffb039e61a3",
+    "url": "/static/css/main.f7706a6d.chunk.css"
+  },
+  {
+    "revision": "f0ebd11ae2b6c8b6a955",
+    "url": "/static/css/1.43ba8683.chunk.css"
+  },
+  {
+    "revision": "38b630518bdb4654df9f233e2f69d14f",
+    "url": "/index.html"
+  }
+];
